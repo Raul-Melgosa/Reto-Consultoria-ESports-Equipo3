11c1bae81895931a3f65ab05ef27fa63c4f4d8d7
Raul Melgosa <73885480+Raul-Melgosa@users.noreply.github.com>
2021-05-12 13:10:41 +0200

    Add files via upload

---
+++ Java/PruebasLoginConModos/src/ModeloUML/Perfil.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ModeloUML;
+
+/**
+ *
+ * @author Equipo 3(Raúl Melgosa, Oier Velar, Alaitz Candela)
+ */
+public class Perfil {
+    private String nombreUsuario;
+    private String password;
+    private String email;
+    private TipoPerfil tipo;
+
+    public Perfil(String nombreUsuario, String password, String email, TipoPerfil tipo) {
+        this.nombreUsuario = nombreUsuario;
+        this.password = password;
+        this.email = email;
+        this.tipo = tipo;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public TipoPerfil getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoPerfil tipo) {
+        this.tipo = tipo;
+    }
+
+    
+}
